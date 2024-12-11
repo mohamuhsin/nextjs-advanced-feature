8e4441b2bef1788e9ee2a83a73ad442200967bf1
mohamuhsin <muhsinohoho@outlook.com>
2024-12-11 13:29:24 +0300

    changing server to client component

--- components/ClientDemo.js
+++ components/ClientDemo.js
@@ -1,14 +1,17 @@
-export default function RSCDemo() {
-    console.log("RSCDemo rendered");
+"use client";
+
+export default function ClientDemo({ children }) {
+    console.log("ClientDemo rendered");
     return (
-        <div className="src">
+        <div className="client-cmp">
             <h2>A React Client Component</h2>
             <p>
-                Will be rendered on the client <strong>AND</strong>
+                Will be rendered on the client <strong>AND</strong> the server.
             </p>
             <p>
                 <strong>NEVER</strong> on the client-side!
             </p>
+            {children}
         </div>
     );
 }
